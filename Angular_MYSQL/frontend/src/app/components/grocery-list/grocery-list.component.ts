68a93886b03dee8029d90f2e231ef371e054e3c6
Nomael <45855919+Nomael@users.noreply.github.com>
2023-11-13 21:06:17 +0100

    Update

--- Angular_MYSQL/frontend/src/app/components/grocery-list/grocery-list.component.ts
+++ Angular_MYSQL/frontend/src/app/components/grocery-list/grocery-list.component.ts
@@ -1,15 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Observable } from 'rxjs';
+
+import { GroceryListCrudService } from 'src/app/services/grocery-list-crud.service';
+
+import { Grocery } from 'src/app/models/Grocery';
+
 
 @Component({
   selector: 'app-grocery-list',
   templateUrl: './grocery-list.component.html',
   styleUrls: ['./grocery-list.component.scss']
 })
-export class GroceryListComponent {
+export class GroceryListComponent implements OnInit {
+  groceries$: Observable<Grocery[]> = new Observable<Grocery[]>();
 
-  groceries = [
-    { id: 1, item: 'bread'},
-    { id: 2, item: 'milk'}
-  ];
+  constructor(private groceryListCrudService: GroceryListCrudService) { }
+
+  ngOnInit(): void {
+    this.groceries$ = this.groceryListCrudService.fetchAll();
+  }
+
+  post(groceryItem: Partial<Grocery>): void {
+    const item = (<string>groceryItem).trim();
+    if (!item) return; 
+    
+    // this.groceries$ = this.groceryListCrudService.post()
+
+
+  }
 
 }
--- Angular_MYSQL/frontend/src/app/services/grocery-list-crud.service.ts
+++ Angular_MYSQL/frontend/src/app/services/grocery-list-crud.service.ts
@@ -1,11 +1,34 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from '@angular/core';
 
+import { Observable } from "rxjs";
+import { catchError, tap } from "rxjs/operators";
+
+
 import { Grocery } from "../models/Grocery";
+import { ErrorHandlerService } from "./error-handler.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class GroceryListCrudService {
-  constructor(private http: HttpClient) { }
+  private url = "http://localhost:3000/groceries";
+
+  constructor(
+      private errorHandlerService: ErrorHandlerService, 
+      private http: HttpClient
+    ) { }
+
+  fetchAll(): Observable<Grocery[]> {
+    return this.http
+      .get<Grocery[]>(this.url, {responseType: "json"})
+      .pipe(
+        tap((_) => console.log('fetched groceries')),
+        catchError(
+          this.errorHandlerService.handleError<Grocery[]>('fetchAll', [])
+        )
+    );
+  }
+
+  
 }
